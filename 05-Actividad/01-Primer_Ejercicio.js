319ef8a2f271417152c487717a07979f1436c69a
Manuel8973 <mineriajomi@gmail.com>
2025-03-06 10:00:39 -0400

    tarea

---
+++ 05-Actividad/01-Primer_Ejercicio.js
@@ -0,0 +1,10 @@
+function contarParesImpares(arr) {
+    let resultado = { pares: 0, impares: 0 };
+    arr.forEach(num => {
+        if (num % 2 === 0) resultado.pares++;
+        else resultado.impares++;
+    });
+    return resultado;
+}
+
+console.log(contarParesImpares([1, 2, 3, 4, 5, 6]));
